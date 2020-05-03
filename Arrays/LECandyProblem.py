31b5aa9699242e98658e708d1bf3af19309b40a0
Ashwani Solanki <ashwanisolanki@in.ibm.com>
2020-05-03 10:14:46 +0530

    Some new problems

--- Arrays/LECandyProblem.py
+++ Arrays/LECandyProblem.py
@@ -1,3 +1,13 @@
+""" A Little Elephant and his friends from the Zoo of Lviv like candies very much.
+There are N elephants in the Zoo. The elephant with number K (1 ≤ K ≤ N) will be happy if he receives at least AK candies. There are C candies in all in the Zoo.
+
+The Zoo staff is interested in knowing whether it is possible to make all the N elephants happy by giving each elephant at least as many candies as he wants, that is, 
+the Kth elephant should receive at least AK candies. Each candy can be given to only one elephant. Print Yes if it is possible and No otherwise. """
+
+""" The first line of the input file contains an integer T, the number of test cases. T test cases follow. Each test case consists of exactly 2 lines. 
+The first line of each test case contains two space separated integers N and C, the total number of elephants and the total number of candies in the Zoo respectively. 
+The second line contains N space separated integers A1, A2, ..., AN. """
+
 import array, math
 
 def check_elephant_happiness(arr_candy_req_by_elephants, num_of_elephants, num_of_candies):
